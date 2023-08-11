13ba9a1ba19e14f2709159d2a59c2f12774a381a
ericaskari <ericaskari@gmail.com>
2023-08-11 11:19:40 +0300

    HomePage product list

--- CBTD/Pages/Index.cshtml.cs
+++ CBTD/Pages/Index.cshtml.cs
@@ -1,17 +1,28 @@
-﻿namespace CBTD.Pages;
+﻿using Infrastructure;
+using Infrastructure.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CBTD.Pages;
 
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    public IEnumerable<Product> objProductList;
+    public IEnumerable<Category> objCategoryList { get; set; }
+    private readonly UnitOfWork _unitOfWork;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, UnitOfWork unitOfWork)
     {
+        _unitOfWork = unitOfWork;
         _logger = logger;
     }
 
-    public void OnGet()
+    public IActionResult OnGet()
     {
+        objCategoryList = _unitOfWork.Category.GetAll(null, c => c.DisplayOrder, null);
+        objProductList = _unitOfWork.Product.GetAll(null, includes: "Category,Manufacturer");
+        return Page();
     }
 }
