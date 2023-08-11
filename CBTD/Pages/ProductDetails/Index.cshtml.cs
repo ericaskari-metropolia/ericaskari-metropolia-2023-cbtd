71c39104bd950015da2f7d5b218f2d8819573a29
ericaskari <ericaskari@gmail.com>
2023-08-11 11:54:04 +0300

    Product Details Completed

--- CBTD/Pages/ProductDetails/Index.cshtml.cs
+++ CBTD/Pages/ProductDetails/Index.cshtml.cs
@@ -1,11 +1,25 @@
+using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CBTD.Pages.ProductDetails;
 
 public class IndexModel : PageModel
 {
-    public void OnGet()
+    private readonly UnitOfWork _unitOfWork;
+    [BindProperty]
+    public int txtCount { get; set; }
+
+    public Product objProduct { get; set; }
+
+    public IndexModel(UnitOfWork unitOfWork)
     {
-        
+        _unitOfWork = unitOfWork;
+    }
+    public IActionResult OnGet(int? productId)
+    {
+        objProduct = _unitOfWork.Product.Get(p => p.Id == productId, includes: "Category,Manufacturer");
+        return Page();
     }
 }
+
